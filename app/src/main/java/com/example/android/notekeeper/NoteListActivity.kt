8102477d8e13f4310372a8c9bb5864480427c05d
Jorge Hernández <r3b0rn.jhs@gmail.com>
2018-06-29 12:51:03 +0100

    Added new activity file that will show a list of notes of a selected courses and make it the launcher one.

---
+++ app/src/main/java/com/example/android/notekeeper/NoteListActivity.kt
@@ -0,0 +1,23 @@
+package com.example.android.notekeeper
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_note_list.*
+
+class NoteListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_list)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            val activityIntent = Intent(this, MainActivity::class.java)
+            startActivity(activityIntent)
+        }
+    }
+
+}
