144db248a388923470a7244a26396e2febc4cae0
Jorge Hernández <r3b0rn.jhs@gmail.com>
2018-06-29 12:36:38 +0100

    Deleted generic design for content_main and set up a spinner of courses and edit type note texts

--- app/src/main/java/com/example/android/notekeeper/NoteKeeperData.kt
+++ app/src/main/java/com/example/android/notekeeper/NoteKeeperData.kt
@@ -1,5 +1,10 @@
 package com.example.android.notekeeper
 
-class CourseInfo (val courseId : String, val title : String)
+class CourseInfo (val courseId : String, val title : String) {
+    override fun toString(): String {
+        return title
+    }
+}
 
-class NoteInfo (var course : CourseInfo, var title : String, var text : String){}
+class NoteInfo (var course : CourseInfo, var title : String, var text : String)
+
